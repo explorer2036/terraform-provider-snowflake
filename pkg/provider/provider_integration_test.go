ed96653e4eace71023a1c61be35e047eda82aa33
explorer2036 <explorer2036@163.com>
2023-10-30 23:24:20 +0800

    add integration test for key pair

--- pkg/provider/provider_integration_test.go
+++ pkg/provider/provider_integration_test.go
@@ -1,6 +1,11 @@
 package provider
 
 import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"testing"
@@ -51,23 +56,41 @@ password='%s'
 		require.NoError(t, err)
 		require.NoError(t, c.Ping())
 	})
-}
 
-func TestInt_KeyPair(t *testing.T) {
-	data, err := os.ReadFile("/home/longwu/.ssh/snowflake_key")
-	require.NoError(t, err)
-	testPrivKey, err := parsePrivateKey(data, nil)
-	require.NoError(t, err)
+	t.Run("auth with key pair", func(t *testing.T) {
+		key, err := rsa.GenerateKey(rand.Reader, 2048)
+		require.NoError(t, err)
+		publicKey, err := x509.MarshalPKIXPublicKey(key.Public())
+		require.NoError(t, err)
+		c1, err := sdk.NewClient(&gosnowflake.Config{
+			User:     user,
+			Account:  account,
+			Password: password,
+		})
+		require.NoError(t, err)
+		encoded := base64.StdEncoding.EncodeToString(publicKey)
+		options := sdk.AlterUserOptions{
+			Set: &sdk.UserSet{
+				ObjectProperties: &sdk.UserObjectProperties{
+					RSAPublicKey: sdk.String(encoded),
+				},
+			},
+		}
+		id := sdk.NewAccountObjectIdentifier(user)
+		err = c1.Users.Alter(context.Background(), id, &options)
+		require.NoError(t, err)
 
-	// testPrivKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	// require.NoError(t, err)
+		u, err := c1.Users.Describe(context.Background(), id)
+		require.NoError(t, err)
+		require.Equal(t, encoded, u.RsaPublicKey.Value)
 
-	c, err := sdk.NewClient(&gosnowflake.Config{
-		User:          "TERRAFORM_SVC_ACCOUNT",
-		Account:       "YXA30390",
-		Authenticator: gosnowflake.AuthTypeJwt,
-		PrivateKey:    testPrivKey,
+		c2, err := sdk.NewClient(&gosnowflake.Config{
+			User:          user,
+			Account:       account,
+			Authenticator: gosnowflake.AuthTypeJwt,
+			PrivateKey:    key,
+		})
+		require.NoError(t, err)
+		require.NoError(t, c2.Ping())
 	})
-	require.NoError(t, err)
-	require.NoError(t, c.Ping())
 }
