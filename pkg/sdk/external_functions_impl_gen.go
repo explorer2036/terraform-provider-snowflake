212ab41b66d246b315e9b1de49de959c1b4fae13
alonlong <alonlong@163.com>
2024-01-25 10:45:49 +0800

    fix the external function

--- pkg/sdk/external_functions_impl_gen.go
+++ pkg/sdk/external_functions_impl_gen.go
@@ -52,29 +52,21 @@ func (r *CreateExternalFunctionRequest) toOpts() *CreateExternalFunctionOptions 
 	if r.Arguments != nil {
 		s := make([]ExternalFunctionArgument, len(r.Arguments))
 		for i, v := range r.Arguments {
-			s[i] = ExternalFunctionArgument{
-				ArgName:     v.ArgName,
-				ArgDataType: v.ArgDataType,
-			}
+			s[i] = ExternalFunctionArgument(v)
 		}
 		opts.Arguments = s
 	}
 	if r.Headers != nil {
 		s := make([]ExternalFunctionHeader, len(r.Headers))
 		for i, v := range r.Headers {
-			s[i] = ExternalFunctionHeader{
-				Name:  v.Name,
-				Value: v.Value,
-			}
+			s[i] = ExternalFunctionHeader(v)
 		}
 		opts.Headers = s
 	}
 	if r.ContextHeaders != nil {
 		s := make([]ExternalFunctionContextHeader, len(r.ContextHeaders))
 		for i, v := range r.ContextHeaders {
-			s[i] = ExternalFunctionContextHeader{
-				ContextFunction: v.ContextFunction,
-			}
+			s[i] = ExternalFunctionContextHeader(v)
 		}
 		opts.ContextHeaders = s
 	}
@@ -99,19 +91,14 @@ func (r *AlterExternalFunctionRequest) toOpts() *AlterExternalFunctionOptions {
 		if r.Set.Headers != nil {
 			s := make([]ExternalFunctionHeader, len(r.Set.Headers))
 			for i, v := range r.Set.Headers {
-				s[i] = ExternalFunctionHeader{
-					Name:  v.Name,
-					Value: v.Value,
-				}
+				s[i] = ExternalFunctionHeader(v)
 			}
 			opts.Set.Headers = s
 		}
 		if r.Set.ContextHeaders != nil {
 			s := make([]ExternalFunctionContextHeader, len(r.Set.ContextHeaders))
 			for i, v := range r.Set.ContextHeaders {
-				s[i] = ExternalFunctionContextHeader{
-					ContextFunction: v.ContextFunction,
-				}
+				s[i] = ExternalFunctionContextHeader(v)
 			}
 			opts.Set.ContextHeaders = s
 		}
@@ -139,6 +126,28 @@ func (r *ShowExternalFunctionRequest) toOpts() *ShowExternalFunctionOptions {
 }
 
 func (r externalFunctionRow) convert() *ExternalFunction {
-	// TODO: Mapping
-	return &ExternalFunction{}
+	e := &ExternalFunction{
+		CreatedOn:          r.CreatedOn,
+		Name:               r.Name,
+		SchemaName:         r.SchemaName,
+		IsBuiltin:          r.IsBuiltin == "Y",
+		IsAggregate:        r.IsAggregate == "Y",
+		IsAnsi:             r.IsAnsi == "Y",
+		MinNumArguments:    r.MinNumArguments,
+		MaxNumArguments:    r.MaxNumArguments,
+		Arguments:          r.Arguments,
+		Description:        r.Description,
+		CatalogName:        r.CatalogName,
+		IsTableFunction:    r.IsTableFunction == "Y",
+		ValidForClustering: r.ValidForClustering == "Y",
+		IsExternalFunction: r.IsExternalFunction == "Y",
+		Language:           r.Language,
+	}
+	if r.IsSecure.Valid {
+		e.IsSecure = r.IsSecure.String == "Y"
+	}
+	if r.IsMemoizable.Valid {
+		e.IsMemoizable = r.IsMemoizable.String == "Y"
+	}
+	return e
 }
