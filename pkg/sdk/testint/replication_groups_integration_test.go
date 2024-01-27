43890ea7a5d2592c1108cb7a87ba6077642d3fc4
explorer2036 <explorer2036@163.com>
2024-01-27 15:01:48 +0800

    add integration tests

--- pkg/sdk/testint/replication_groups_integration_test.go
+++ pkg/sdk/testint/replication_groups_integration_test.go
@@ -1,6 +1,8 @@
 package testint
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
@@ -19,6 +21,31 @@ func TestInt_ReplicationGroups(t *testing.T) {
 		}
 	}
 
+	assertReplicationGroup := func(t *testing.T, id sdk.AccountObjectIdentifier, objectTypes []string, org string, account string, schedule string) {
+		t.Helper()
+
+		e, err := client.ReplicationGroups.ShowByID(ctx, id)
+		require.NoError(t, err)
+		primary := fmt.Sprintf("%s.%s.%s", org, account, id.Name())
+		require.NotEmpty(t, e.CreatedOn)
+		require.Equal(t, "AWS_US_WEST_2", e.SnowflakeRegion)
+		require.Equal(t, account, e.AccountName)
+		require.Equal(t, id.Name(), e.Name)
+		require.Equal(t, "REPLICATION", e.Type)
+		require.Empty(t, e.Comment)
+		require.Equal(t, false, e.IsPrimary)
+		require.Equal(t, primary, e.Primary)
+		require.Equal(t, strings.Join(objectTypes, ", "), e.ObjectTypes)
+		require.Empty(t, e.AllowedIntegrationTypes)
+		require.NotEmpty(t, e.AllowedAccounts)
+		require.Equal(t, org, e.OrganizationName)
+		require.NotEmpty(t, e.AccountLocator)
+		require.Equal(t, schedule, e.ReplicationSchedule)
+		require.Empty(t, e.SecondaryState)
+		require.Empty(t, e.NextScheduledRefresh)
+		require.Equal(t, "ACCOUNTADMIN", e.Owner)
+	}
+
 	t.Run("create replication group", func(t *testing.T) {
 		db1, cleanupDb1 := createDatabase(t, client)
 		t.Cleanup(cleanupDb1)
@@ -30,6 +57,7 @@ func TestInt_ReplicationGroups(t *testing.T) {
 		t.Cleanup(cleanupShare2)
 
 		id := sdk.NewAccountObjectIdentifier(random.AlphaN(5))
+		objectTypes := []string{"DATABASES", "SHARES"}
 		ot := sdk.NewReplicationGroupObjectTypesRequest().
 			WithDatabases(sdk.Bool(true)).
 			WithShares(sdk.Bool(true))
@@ -41,8 +69,9 @@ func TestInt_ReplicationGroups(t *testing.T) {
 			sdk.NewAccountObjectIdentifier(share1.Name.Name()),
 			sdk.NewAccountObjectIdentifier(share2.Name.Name()),
 		}
+		org, account := "SFDEVREL", "CLOUD_ENGINEERING"
 		as := []sdk.AccountObjectIdentifier{
-			sdk.NewAccountObjectIdentifier("SFDEVREL.CLOUD_ENGINEERING2"),
+			sdk.NewAccountObjectIdentifier(org + "." + account),
 		}
 		schedule := sdk.NewReplicationGroupScheduleRequest().WithIntervalMinutes(sdk.NewScheduleIntervalMinutesRequest(10))
 		request := sdk.NewCreateReplicationGroupRequest(id).
@@ -54,5 +83,7 @@ func TestInt_ReplicationGroups(t *testing.T) {
 		err := client.ReplicationGroups.Create(ctx, request)
 		require.NoError(t, err)
 		t.Cleanup(cleanupReplicationGroupHandle(id))
+
+		assertReplicationGroup(t, id, objectTypes, org, account, "10 MINUTES")
 	})
 }
