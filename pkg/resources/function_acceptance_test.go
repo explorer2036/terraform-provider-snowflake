a301bf894ddb3291151f39bd9fcb74763c9ab7b2
alonlong <alonlong@163.com>
2024-02-06 16:16:58 +0800

    add acc tests for function.

--- pkg/resources/function_acceptance_test.go
+++ pkg/resources/function_acceptance_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 
 	acc "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance"
+	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func TestAcc_Function(t *testing.T) {
@@ -130,4 +132,70 @@ select 3, 4
 EOT
 	}
 	`, name, databaseName, schemaName, name, databaseName, schemaName, name, databaseName, schemaName, name, databaseName, schemaName)
+}
+
+func testAccFunction(t *testing.T) {
+	t.Helper()
+
+	name := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	resourceName := "snowflake_function.f"
+	m := func() map[string]config.Variable {
+		return map[string]config.Variable{
+			"name":     config.StringVariable(name),
+			"database": config.StringVariable(acc.TestDatabaseName),
+			"schema":   config.StringVariable(acc.TestSchemaName),
+			"comment":  config.StringVariable("Terraform acceptance test"),
+		}
+	}
+	variableSet2 := m()
+	variableSet2["comment"] = config.StringVariable("Terraform acceptance test - updated")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
+		PreCheck:                 func() { acc.TestAccPreCheck(t) },
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.RequireAbove(tfversion.Version1_5_0),
+		},
+		CheckDestroy: testAccCheckDynamicTableDestroy,
+		Steps: []resource.TestStep{
+			{
+				ConfigDirectory: config.TestStepDirectory(),
+				ConfigVariables: m(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "database", acc.TestDatabaseName),
+					resource.TestCheckResourceAttr(resourceName, "schema", acc.TestSchemaName),
+					resource.TestCheckResourceAttr(resourceName, "comment", "Terraform acceptance test"),
+
+					// computed attributes
+					resource.TestCheckResourceAttrSet(resourceName, "null_input_behavior"),
+					resource.TestCheckResourceAttrSet(resourceName, "return_type"),
+					resource.TestCheckResourceAttrSet(resourceName, "statement"),
+					resource.TestCheckResourceAttrSet(resourceName, "execute_as"),
+					resource.TestCheckResourceAttrSet(resourceName, "secure"),
+				),
+			},
+
+			// test - change comment
+			{
+				ConfigDirectory: config.TestStepDirectory(),
+				ConfigVariables: variableSet2,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "database", acc.TestDatabaseName),
+					resource.TestCheckResourceAttr(resourceName, "schema", acc.TestSchemaName),
+					resource.TestCheckResourceAttr(resourceName, "comment", "Terraform acceptance test - updated"),
+				),
+			},
+
+			// test - import
+			// {
+			// 	ConfigDirectory:   config.TestStepDirectory(),
+			// 	ConfigVariables:   variableSet2,
+			// 	ResourceName:      resourceName,
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// },
+		},
+	})
 }
