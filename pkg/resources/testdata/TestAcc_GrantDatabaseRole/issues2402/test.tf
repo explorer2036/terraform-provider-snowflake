b533680381b8656a194086af310ff5abdf89994a
alonlong <alonlong@163.com>
2024-02-15 21:21:28 +0800

    fix the issue 2402

---
+++ pkg/resources/testdata/TestAcc_GrantDatabaseRole/issues2402/test.tf
@@ -0,0 +1,30 @@
+variable "database_role_name" {
+  type = string
+}
+
+variable "parent_database_role_name" {
+  type = string
+}
+
+variable "database" {
+  type = string
+}
+
+resource "snowflake_database" "database" {
+  name = var.database
+}
+
+resource "snowflake_database_role" "database_role" {
+  database = snowflake_database.database.name
+  name     = var.database_role_name
+}
+
+resource "snowflake_database_role" "parent_database_role" {
+  database = snowflake_database.database.name
+  name     = var.parent_database_role_name
+}
+
+resource "snowflake_grant_database_role" "g" {
+  database_role_name        = "\"${snowflake_database.database.name}\".\"${snowflake_database_role.database_role.name}\""
+  parent_database_role_name = "\"${snowflake_database.database.name}\".\"${snowflake_database_role.parent_database_role.name}\""
+}
